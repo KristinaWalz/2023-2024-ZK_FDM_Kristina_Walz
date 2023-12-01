cd5e856182496115afed063ab801de26db7ec612
Kristina Walz <kristina.walz@uni-bielefeld.de>
2023-12-01 15:26:17 +0100

    Initial commit

---
+++ count_entries.sh
@@ -0,0 +1,7 @@
+# A script to count all lines in a TSV file
+# 2023 by Kristina Walz
+# MIT license
+
+echo "Counting the number of lines in all TSV-files"
+wc -l *tsv
+echo "Done. Have a nice day :-) "
